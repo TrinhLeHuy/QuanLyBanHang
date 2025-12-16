f41d79d953e4794b551f4e2d89f9d77c86a2fd9c
Retsera <3122410309.sv.sgu.edu.vn>
2025-12-16 08:20:28 +0700

    123

--- QuanLyBanHang.Web/obj/Debug/net9.0/QuanLyBanHang.Web.AssemblyInfo.cs
+++ QuanLyBanHang.Web/obj/Debug/net9.0/QuanLyBanHang.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanLyBanHang.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70c4979a19dd9552fce096e00b406261ab1a44d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1988fcf1db9fdb707697257cfbabdcdcae1e96f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanLyBanHang.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuanLyBanHang.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
